be2738ce2839d5e97f3eb2851fd667bd1922599d
npartovi <npartovi@gmail.com>
2018-06-26 08:52:44 -0700

    refactored context api

---
+++ context_api/src/components/App.js
@@ -0,0 +1,71 @@
+import React, { Component, Fragment } from 'react';
+
+const MyContext = React.createContext();
+
+//then create a provider component
+class MyProvider extends Component {
+  state = {
+    name:  'nima',
+    age: 31,
+    cool: false,
+
+  }
+
+  render(){
+    return(
+      <MyContext.Provider value={{
+        state: this.state,
+        increaseAge: () => this.setState({age: this.state.age + 1})
+      }}>
+          {this.props.children}
+      </MyContext.Provider>
+    )
+  }
+}
+
+class App extends Component {
+  
+  render() {
+    return (
+      <MyProvider>
+      <div>
+        <p>This is the App Component</p>
+      </div>
+      </MyProvider>
+      
+    );
+  }
+}
+
+
+// const Family = (props) => (
+//   <div className="family">
+//     <Person />
+//   </div>
+// )
+
+// class Person extends Component {
+  
+//     render(){
+//       return(
+//         <div className="person">
+//             <MyContext.Consumer>
+//               {(context) => (
+//                 <Fragment>
+//                   <p>{context.state.name}</p>
+//                   <p>{context.state.age}</p>
+//                   <button onClick={context.increaseAge}>increase age</button>
+//                 </Fragment>
+                
+//               )}
+//             </MyContext.Consumer>
+//         </div>
+//       )
+
+//   }
+// }
+
+
+
+
+export default App;
