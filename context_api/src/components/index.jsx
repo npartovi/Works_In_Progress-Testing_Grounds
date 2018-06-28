0fbdd42f57f9b3f1b96203e21ae067c041a6db04
npartovi <npartovi@gmail.com>
2018-06-28 13:25:28 -0700

    pulling in random channels now

--- context_api/src/components/index.jsx
+++ context_api/src/components/index.jsx
@@ -1,27 +1,76 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import Keys from './keys/keys'
 
 class Index extends Component {
     constructor(props){
         super(props)
 
         this.state = {
-            videoList: ""
+            gamesList: [],
+            randomChannel: ""
         }
+
+        this.randomStream = this.randomStream.bind(this)
     }
 
+
+    // componentDidUpdate(){
+        
+    //     let results = []
+    //     this.state.videoList.forEach((game) => {
+    //         // console.log(game.game.name)
+    //         results.push(game.game.name)
+    //     })        
+    // }
 
     componentDidMount(){
-        axios.get('https://api.twitch.tv/helix/streams?first=20',{'headers': {'Client-ID': '2bjd68f3pi5ak6nn0ch2sk60x4kkfu'}})
-            .then( res => {
-                this.setState({videoList: res.data})
+        let results = []
+        axios.get('https://api.twitch.tv/kraken/games/top?limit=100' ,{'headers': {'Client-ID': Keys.twitch }})
+            .then(res => {
+                res.data.top.forEach(game => {
+                    results.push(game.game.name)
+                })
             })
+        this.setState({gamesList: results})
     }
 
+    randomStream(){
+        let randomGame = this.state.gamesList[Math.floor(Math.random() * this.state.gamesList.length)]
+
+        let randomChannel = ""
+        axios.get('https://api.twitch.tv/kraken/streams?limit=20&game='+ this.state.randomGame, {'headers': {'Client-ID': Keys.twitch }})
+            .then(res => {
+                randomChannel = res.data.streams[Math.floor(Math.random() * res.data.streams.length)]
+            });
+
+        this.setState({randomChannel: randomChannel})
+        
+        
+    
+    }
+
+    // randomChannel(){
+    //     let randomChannel = ""
+    //     axios.get('https://api.twitch.tv/kraken/streams?limit=20&game='+ this.state.randomGame, {'headers': {'Client-ID': Keys.twitch }})
+    //         .then(res => {
+    //             randomChannel = res.data.streams[Math.floor(Math.random() * res.data.streams.length)]
+    //         });
+
+    //     this.setState({randomChannel: randomChannel})
+    // }
+
+
     render(){
-        console.log(this.state.videoList)
+        console.log(this.state)
         return(
-            <p>This is the Index page</p>
+            <div>
+                random video
+                <button onClick={this.randomStream}>Click here for a random stream</button>
+                <div>
+                    This is where the stream will render
+                </div>
+            </div>
         )
     }
 }
