275b42d3b7f495645d26e4741a446ef206122283
npartovi <npartovi@gmail.com>
2018-08-07 16:25:25 -0700

    #4 section done

--- react-tutorials/github-battle/app/components/app.js
+++ react-tutorials/github-battle/app/components/app.js
@@ -1,17 +1,31 @@
-import React from 'react'
-import Popular from './popular'
+import React from 'react';
+import Popular from './popular';
+import Nav from './nav';
+import Home from './home';
+import Battle from './battle'
 
 const ReactRouter = require('react-router-dom')
 const Router = ReactRouter.BrowserRouter;
 const Route = ReactRouter.Route;
+const Switch = ReactRouter.Switch;
 
 class App extends React.Component {
 
     render(){
         return(
+            <Router>
             <div className="container">
-                <Popular />
+                <Nav />
+                <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/popular" component={Popular} />
+                <Route exact path="/battle" component={Battle} />
+                <Route render={() =>{
+                    return(<p>URL NOT FOUND</p>)
+                }} />
+                </Switch>
             </div>
+            </Router>
         )
     }
 }
---
+++ react-tutorials/github-battle/app/components/battle.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Battle extends React.Component {
+
+    render(){
+        return(
+            <div>
+                BATTLE!
+            </div>
+        )
+    }
+}
+
+export default Battle
