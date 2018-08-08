87c9d2526725ab2cb6031b1812585e9e6c60be98
npartovi <npartovi@gmail.com>
2018-08-07 18:41:11 -0700

    #7 section done

--- react-tutorials/github-battle/app/util/api.js
+++ react-tutorials/github-battle/app/util/api.js
@@ -1,5 +1,63 @@
 import axios from 'axios';
 
+const id = "YOUR_CLIENT_ID";
+const sec = "YOUR_SECRET_ID";
+const params = "?client_id=" + id + '&client_secret=' + sec
+
+const getProfile = (username) => {
+    return axios.get('https://api.github.com/users/' + username + params)
+            .then((user) => {
+                return user.data
+            })
+}
+
+const getRepos = (username) => {
+    return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100');
+}
+
+const getStarCount = (repos) => {
+    return repos.data.reduce((count,repo) => {
+        return count + repo.stargazers_count
+    }, 0)
+}
+
+const calculateScore = (profile, repos) => {
+    let followers = profile.followers
+    let totalStars = getStarCount(repos)
+
+    return( followers * 3) + totalStars
+}
+
+const handleErrors = (errors) => {
+    console.warn(errors)
+    return null;
+}
+
+const getUserData = (player) => {
+    return axios.all([
+        getProfile(player),
+        getRepos(player)
+    ]).then((data) => {
+        let profile = data[0];
+        let repos = data[1];
+
+        return {
+            profile: profile,
+            score: calculateScore(profile, repos)
+        }
+    })
+}
+
+const sortPlayers = (players) => {
+    return players.sort((a,b) => {
+        return b.score - a.score
+    })
+
+}
+
+
+
+
 export const fetchPopularRepos = (language) => {
 
     const encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
@@ -8,4 +66,11 @@ export const fetchPopularRepos = (language) => {
         .then((response) => {
             return response.data.items
         })
+}
+
+export const battle = (players) => {
+    return axios.all(players.map(getUserData))
+        .then(sortPlayers)
+        .catch(handleErrors)
+
 }
