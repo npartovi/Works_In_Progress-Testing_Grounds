06a224d156744c5ea43be33d0ae0b1d0747138d5
npartovi <npartovi@gmail.com>
2018-08-07 17:05:53 -0700

    #5 section done

--- react-tutorials/github-battle/app/components/battle.js
+++ react-tutorials/github-battle/app/components/battle.js
@@ -1,11 +1,101 @@
 import React from 'react'
 
-class Battle extends React.Component {
+
+class PlayerInput extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            username: '',
+
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(event){
+        const value = event.target.value
+
+        this.setState({username: value})
+    }
+
+    handleSubmit(event){
+        event.preventDefault()
+
+        this.props.onSubmit(this.props.id, this.props.username)
+    }
 
     render(){
         return(
+            <form className="column" onSubmit={this.handleSubmit}> 
+                <label className="header" htmlFor="username">
+                    {this.props.label}
+                </label>
+                <input
+                    id="username"
+                    placeholder="github username"
+                    type="text"
+                    autoComplete="off"
+                    value={this.state.username}
+                    onChange={this.handleChange}
+                />
+                <button
+                    className="button"
+                    type="submit"
+                    disabled={!this.state.username}>
+                    Submit
+                </button>
+            </form>
+
+        )
+    }
+}
+
+class Battle extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            playerOneName: "",
+            playerTwoName: "",
+            playerOneImage: null,
+            playerTwoImage: null
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleSubmit(id, username){
+
+        this.setState(() => {
+            const newState = {}
+            newState[id + 'Name'] = username
+            newState[id + 'Player'] = 'https://github.com/' + username + '.png?size=200';
+            return newState
+        })
+    }
+
+    render(){
+        const playerOneName = this.state.playerOneName;
+        const playerTwoName = this.state.playerTwoName;
+
+        return(
             <div>
-                BATTLE!
+                <div className="row">
+                    {!playerOneName && 
+                    <PlayerInput 
+                        id="playerOne"
+                        label="Player One"
+                        onSubmit={this.handleSubmit}
+                    />}
+                    {!playerTwoName && 
+                    <PlayerInput 
+                        id="playerTwo"
+                        label="Player Two"
+                        onSubmit={this.handleSubmit}
+                    />}
+                </div>
             </div>
         )
     }
