f8f9cf2460beb18fb2ee253c0be63120ec2e57ba
npartovi <npartovi@gmail.com>
2018-08-08 19:33:44 -0700

    Refactoring code

--- react-tutorials/github-battle/app/components/battle.js
+++ react-tutorials/github-battle/app/components/battle.js
@@ -17,7 +17,6 @@ class PlayerInput extends React.Component{
 
     handleChange(event){
         const value = event.target.value
-
         this.setState({username: value})
     }
 
@@ -28,24 +27,27 @@ class PlayerInput extends React.Component{
     }
 
     render(){
-        console.log(this.state.username)
+        
+        const { username } = this.state
+        const {label} = this.props
+
         return(
             <form className="column" onSubmit={this.handleSubmit}> 
                 <label className="header" htmlFor="username">
-                    {this.props.label}
+                    {label}
                 </label>
                 <input
                     id="username"
                     placeholder="github username"
                     type="text"
                     autoComplete="off"
-                    value={this.state.username}
+                    value={username}
                     onChange={this.handleChange}
                 />
                 <button
                     className="button"
                     type="submit"
-                    disabled={!this.state.username}>
+                    disabled={!username}>
                     Submit
                 </button>
             </form>
@@ -66,37 +68,26 @@ class Battle extends React.Component {
         }
 
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleReset = this.handleReset.bind(this);
+        // this.handleReset = this.handleReset.bind(this);
     }
 
     handleSubmit(id, username){
-
-        this.setState(() => {
-            let newState = {}
-            newState[id + 'Name'] = username;
-            newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200';
-            return newState
+        this.setState({
+            [id + 'Name']: username,
+            [id + 'Image']: 'https://github.com/' + username + '.png?size=200',
         })
     }
 
     handleReset(id){
-
-        this.setState(() => {
-            let newState = {}
-            newState[id + 'Name'] = "";
-            newState[id + 'Image'] = null;
-            return newState
+        this.setState({
+            [id + 'Name']: "",
+            [id + 'Image']: null,
         })
     }
 
     render(){
-        console.log("battle", this.state)
-        const match = this.props.match
-        const playerOneName = this.state.playerOneName;
-        const playerTwoName = this.state.playerTwoName;
-        const playerOneImage = this.state.playerOneImage;
-        const playerTwoImage =this.state.playerTwoImage;
-
+        const {match} = this.props;
+        const {playerOneName, playerTwoName, playerOneImage, playerTwoImage} = this.state;
 
         return(
             <div>
@@ -114,7 +105,7 @@ class Battle extends React.Component {
                     >
                         <button
                             className="reset"
-                            onClick={this.handleReset.bind(null, "playerOne")}>
+                            onClick={() => this.handleReset("playerOne")}>
                         Reset
                         </button>
                     </PlayerPreview>}
@@ -132,7 +123,7 @@ class Battle extends React.Component {
                     >
                         <button
                             className="reset"
-                            onClick={this.handleReset.bind(null, 'playerTwo')}>
+                            onClick={() => this.handleReset('playerTwo')}>
                             Reset
                         </button>
                     </PlayerPreview>
@@ -143,7 +134,7 @@ class Battle extends React.Component {
                             className="button"
                             to={{
                                 pathname: match.url + '/results',
-                                search: '?playerOneName=' + playerOneName + '&playerTwoName=' + playerTwoName
+                                search: `?playerOneName=${playerOneName}&playerTwoName=${playerTwoName}`
                             }}>
                         Battle
                         </Link>}
--- react-tutorials/github-battle/app/util/api.js
+++ react-tutorials/github-battle/app/util/api.js
@@ -2,30 +2,23 @@ import axios from 'axios';
 
 const id = "YOUR_CLIENT_ID";
 const sec = "YOUR_SECRET_ID";
-const params = "?client_id=" + id + '&client_secret=' + sec
+const params = `?client_id=${id}&client_secret=${sec}`
 
 const getProfile = (username) => {
-    return axios.get('https://api.github.com/users/' + username + params)
-            .then((user) => {
-                return user.data
-            })
+    return axios.get(`https://api.github.com/users/${username}${params}`)
+            .then(({data}) =>  data )
 }
 
 const getRepos = (username) => {
-    return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100');
+    return axios.get(`https://api.github.com/users/${username}/repos${params}&per_page=100`);
 }
 
 const getStarCount = (repos) => {
-    return repos.data.reduce((count,repo) => {
-        return count + repo.stargazers_count
-    }, 0)
+    return repos.data.reduce((count,{stargazers_count}) => count + stargazers_count, 0)
 }
 
-const calculateScore = (profile, repos) => {
-    let followers = profile.followers
-    let totalStars = getStarCount(repos)
-
-    return( followers * 3) + totalStars
+const calculateScore = ({followers}, repos) => {
+    return( followers * 3) + getStarCount(repos)
 }
 
 const handleErrors = (errors) => {
@@ -33,44 +26,45 @@ const handleErrors = (errors) => {
     return null;
 }
 
+// const getUserData = (player) => {
+//     return axios.all([
+//         getProfile(player),
+//         getRepos(player)
+//     ]).then((data) => {
+//         let profile = data[0];
+//         let repos = data[1];
+
+//         return {
+//             profile: profile,
+//             score: calculateScore(profile, repos)
+//         }
+//     })
+// }
+
 const getUserData = (player) => {
-    return axios.all([
+    return Promise.all([
         getProfile(player),
         getRepos(player)
-    ]).then((data) => {
-        let profile = data[0];
-        let repos = data[1];
-
-        return {
-            profile: profile,
-            score: calculateScore(profile, repos)
-        }
-    })
+    ]).then(([profile, repos]) => ({
+        profile,
+        score: calculateScore(profile, repos)
+    }))
 }
 
 const sortPlayers = (players) => {
-    return players.sort((a,b) => {
-        return b.score - a.score
-    })
-
+    return players.sort((a,b) => { b.score - a.score})
 }
 
 
 
 
 export const fetchPopularRepos = (language) => {
-
-    const encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
-
-    return axios.get(encodedURI)
-        .then((response) => {
-            return response.data.items
-        })
+    const encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
+    return axios.get(encodedURI).then(({data}) => {data.items})
 }
 
 export const battle = (players) => {
-    return axios.all(players.map(getUserData))
+    return Promise.all(players.map(getUserData))
         .then(sortPlayers)
         .catch(handleErrors)
-
 }
