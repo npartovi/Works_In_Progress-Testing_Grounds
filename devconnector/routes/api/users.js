0707fa45a1f76c5bcd27d740b29da0ac0c06ab7c
npartovi <npartovi@gmail.com>
2018-08-13 18:07:12 -0700

    added models and gravatar

--- devconnector/routes/api/users.js
+++ devconnector/routes/api/users.js
@@ -1,6 +1,40 @@
 const express = require('express')
 const router = express.Router();
+const gravatar = require('gravatar')
 
+
+//Load User Model
+const User = require('../../models/User')
+
+
+// @route GET api/users/test
+// @desc Tests users route
+// @access Public
 router.get('/test', (req, res) => res.json({msg: "Users Works"}))
+
+// @route GET api/users/register
+// @desc Register user 
+// @access Public
+
+router.post('/register', (req, res) => {
+    User.findOne({ email: request.body.email })
+        .then((user) => {
+            if(user){
+                return res.status(400).json({email: 'Email already exists'})
+            }else{
+                const avatar = gravatar.url(req.body.email){
+                    s: 200, // Size
+                    r: "pg", // Rating
+                    d: "mm" //Default
+                }
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar,
+                    password: req.body.password
+                })
+            }
+        })
+})
 
 module.exports = router
