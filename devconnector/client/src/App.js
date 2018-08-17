84dae8be2052ec98c58119bbe85c0d985501a6af
npartovi <npartovi@gmail.com>
2018-08-16 17:01:34 -0700

    changed login and register links, rigging up onchange and onsubmit handlers

--- devconnector/client/src/App.js
+++ devconnector/client/src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import './App.css';
 
 import Navbar from './components/layout/NavBar'
 import Footer from './components/layout/Footer'
 import Landing from './components/layout/Landing'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
---
+++ devconnector/client/src/components/auth/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Login extends Component {
+    render(){
+        return(
+            <h1>this is the login page</h1>
+        )
+    }
+}
+
+
+export default Login
