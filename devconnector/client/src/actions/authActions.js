609ab376aaa8c89e7dcbc5598fd9aec15d2bdc1f
npartovi <npartovi@gmail.com>
2018-08-19 00:03:57 -0700

    finished frontend login routes

--- devconnector/client/src/actions/authActions.js
+++ devconnector/client/src/actions/authActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
-import { GET_ERRORS } from './types'
+import setAuthToken from '../utils/setAuthToken'
+import jwt_decode from 'jwt-decode'
+import { GET_ERRORS, SET_CURRENT_USER } from './types'
 
 // Register User
 export const registeruser = (userData, history) => dispatch => {
@@ -13,3 +15,39 @@ export const registeruser = (userData, history) => dispatch => {
             })
         )
 }
+
+
+// Login User - Get User token
+
+export const loginUser = (userData) => (dispatch) => {
+    axios.post('/api/users/login', userData)
+        .then(res => {
+            // Save to local Storage
+            const { token } = res.data
+            // SET token to ls
+            localStorage.setItem('jwtToken', token)
+            // Set token to Auth header
+            setAuthToken(token)
+            // Decode token to get user data
+            const decoded = jwt_decode(token);
+            // Set current user
+            dispatch(setCurrentUser(decoded))
+
+        })
+        .catch(err =>{
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        })
+}
+
+// Set logged in user
+
+export const setCurrentUser = decoded => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    }
+}
+
