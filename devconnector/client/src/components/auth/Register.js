a64db0ba4e29fa4fed659b31499f02e224ad4fca
npartovi <npartovi@gmail.com>
2018-08-18 17:25:39 -0700

    finished redux for auth

--- devconnector/client/src/components/auth/Register.js
+++ devconnector/client/src/components/auth/Register.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react'
 import axios from 'axios'
+import PropTypes from 'prop-types'
 import classnames from 'classnames';
+import { connect } from 'react-redux';
+import { registeruser } from '../../actions/authActions'
 
 class Register extends Component {
     constructor(props){
@@ -32,16 +35,17 @@ class Register extends Component {
             password2: this.state.password2
         }
 
-        axios.post('/api/users/register', newUser)
-            .then(res => console.log(res.data))
-            .catch(err => this.setState({errors: err.response.data}))
+        this.props.registeruser(newUser)
+
+        // axios.post('/api/users/register', newUser)
+        //     .then(res => console.log(res.data))
+        //     .catch(err => this.setState({errors: err.response.data}))
     }
 
     render(){
 
         const { errors } = this.state
-
-
+        
         return(
             <div className="register">
                 <div className="container">
@@ -105,5 +109,14 @@ class Register extends Component {
     }
 }
 
-export default Register
+Register.propTypes = {
+    registeruser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+    auth: state.auth
+})
+
+export default connect(mapStateToProps, {registeruser})(Register)
 
