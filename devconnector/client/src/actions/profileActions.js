ee04e96b4a9409fe46752bc70da6e81d04a779ae
npartovi <npartovi@gmail.com>
2018-08-22 22:07:12 -0700

    finished add education form

--- devconnector/client/src/actions/profileActions.js
+++ devconnector/client/src/actions/profileActions.js
@@ -37,9 +37,19 @@ export const addExperience = (expData, history) => dispatch => {
                 payload: err.response.data
             })
         )
-
 }
 
+export const addEducation = (eduData, history) => dispatch => {
+    axios
+        .post('/api/profile/education', eduData)
+        .then(res => history.push('/dashboard'))
+        .catch(err => 
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        )
+}
 
 
 
