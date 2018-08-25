cae128fb1136c67aead2fbb49398d74a60f1d1fc
npartovi <npartovi@gmail.com>
2018-08-24 18:09:04 -0700

    add comment functionality

--- devconnector/client/src/reducers/postReducer.js
+++ devconnector/client/src/reducers/postReducer.js
@@ -1,4 +1,4 @@
-import { ADD_POST } from '../actions/types'
+import { ADD_POST, GET_POSTS, POST_LOADING } from '../actions/types'
 
 
 const initialState = {
@@ -9,10 +9,21 @@ const initialState = {
 
 const postReducer = (state = initialState, action) => {
     switch(action.type){
+        case POST_LOADING:
+            return{
+                ...state,
+                loading: true
+            }
         case ADD_POST:
             return{
                 ...state,
                 posts: [action.payload, ...state.posts]
+            }
+        case GET_POSTS:
+            return{
+                ...state,
+                posts: action.payload,
+                loading: false
             }
         default:
             return state
