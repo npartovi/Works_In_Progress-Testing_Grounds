62edf62018c5a4e3788bc00cb520a5b5bb563cec
npartovi <npartovi@gmail.com>
2018-08-24 17:01:33 -0700

    finished add post

--- devconnector/client/src/reducers/postReducer.js
+++ devconnector/client/src/reducers/postReducer.js
@@ -1,4 +1,4 @@
-
+import { ADD_POST } from '../actions/types'
 
 
 const initialState = {
@@ -9,7 +9,11 @@ const initialState = {
 
 const postReducer = (state = initialState, action) => {
     switch(action.type){
-        
+        case ADD_POST:
+            return{
+                ...state,
+                posts: [action.payload, ...state.posts]
+            }
         default:
             return state
     }
