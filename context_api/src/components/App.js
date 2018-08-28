6b51d032e7c345f79bfea30b7f65e07b1d0eff21
npartovi <npartovi@gmail.com>
2018-08-27 23:25:13 -0700

    removed a folder

--- context_api/src/components/App.js
+++ context_api/src/components/App.js
@@ -3,39 +3,15 @@ import TwitchStream from './twitchStreams';
 import YoutubeIndex from './youtubeStreams';
 import './styles/app.css'
 
-const MyContext = React.createContext();
 
-class MyProvider extends Component {
-  state = {
-    name:  'nima',
-    age: 31,
-    cool: false,
-
-  }
-
-  render(){
-    return(
-      <MyContext.Provider value={{
-        state: this.state,
-        increaseAge: () => this.setState({age: this.state.age + 1})
-      }}>
-          {this.props.children}
-      </MyContext.Provider>
-    )
-  }
-}
 
 class App extends Component {
-  
   render() {
     return (
-      <MyProvider>
       <div className="main-grid-container">
         <TwitchStream />
         <YoutubeIndex />
       </div>
-      </MyProvider>
-      
     );
   }
 }
