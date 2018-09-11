c5aedc8939e7c5f924eaded6ee77ac0df1d2865c
npartovi <npartovi@gmail.com>
2018-09-10 17:08:37 -0700

    added filtering of users

--- dom_manip/1-adding-and-removing-names/1-tour-the-application/start/app.js
+++ dom_manip/1-adding-and-removing-names/1-tour-the-application/start/app.js
@@ -2,6 +2,41 @@ const form = document.querySelector('form')
 const input = form.querySelector('input')
 const ul = document.getElementById('invitedList')
 
+const div = document.createElement('div')
+const main = document.querySelector('.main')
+
+const filterLabel = document.createElement('label')
+const filterCheckbox = document.createElement('input')
+
+main.insertBefore(div, ul)
+filterCheckbox.type = "checkbox"
+filterLabel.textContent = "Hide all users who have not confirmed"
+filterLabel.appendChild(filterCheckbox)
+div.appendChild(filterLabel)
+
+
+filterCheckbox.addEventListener('change',(e)=> {
+    const isChecked = e.target.checked
+    const lis = ul.children
+
+    if(isChecked){
+        for(let i = 0; i < lis.length; i++){
+            let li = lis[i]
+            if(li.className === "responded"){
+                li.style.display = ""
+            }else{
+                li.style.display = "none"
+            }
+        }
+    }else{
+        for(let i = 0; i < lis.length; i++){
+            let li = lis[i]
+            li.style.display = ""
+        }
+    }
+})
+
+
 form.addEventListener('submit', (e)=> {
     e.preventDefault()
     
@@ -15,16 +50,26 @@ form.addEventListener('submit', (e)=> {
 
 function createLI(text){
     const li = document.createElement('li')
-    li.innerHTML = text
+    const span = document.createElement('span')
+    span.textContent = text
+    li.appendChild(span)
+    
     const label = document.createElement('label')
     label.textContent = "Confirmed"
+
     const checkbox = document.createElement('input')
     checkbox.type = "checkbox"
     label.appendChild(checkbox)
     li.appendChild(label)
-    const button = document.createElement('button')
-    button.textContent = "remove"
-    li.appendChild(button)
+
+    const removeButton = document.createElement('button')
+    removeButton.textContent = "remove"
+    li.appendChild(removeButton)
+
+    const editButton = document.createElement('button')
+    editButton.textContent = "edit"
+    li.appendChild(editButton)
+
     return li
 }
 
@@ -45,13 +90,34 @@ ul.addEventListener('change',(e) => {
 })
 
 ul.addEventListener('click', (e) => {
-    
    if(e.target.tagName === 'BUTTON'){
-       const li = e.target.parentNode
-       const ul = li.parentNode
-       ul.removeChild(li)
+        const button = e.target
+        const li = e.target.parentNode
+        const ul = li.parentNode
+
+        if(button.textContent === "remove"){  
+           ul.removeChild(li)
+        }else if(button.textContent === "edit"){
+            const span = li.firstElementChild
+            const input = document.createElement('input')
+            input.type = "text"
+            input.value = span.textContent
+            li.insertBefore(input, span)
+            li.removeChild(span)
+            button.textContent = "save"
+       }else if(button.textContent === "save"){
+            const input = li.firstElementChild
+            const span = document.createElement('span')
+            span.textContent = input.value
+            li.insertBefore(span, input)
+            li.removeChild(input)
+            button.textContent = "edit"
+
+       }
    }
-})
+});
+
+
 
 
 
