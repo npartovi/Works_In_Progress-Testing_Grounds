04b1831e058c0aafa4edb97801e3f37e26081048
npartovi <npartovi@gmail.com>
2018-09-24 19:35:10 -0700

    added live links to videos

--- fgcvideos/client/src/VideoListItem.jsx
+++ fgcvideos/client/src/VideoListItem.jsx
@@ -4,11 +4,15 @@ import React from 'react'
 const VideoListItem = ({video}) => {
     console.log(video)
     const imgUrl = video.snippet.thumbnails.medium.url
+    const videoId = video.id.videoId
+    const videoUrl = `https://www.youtube.com/embed/${videoId}`;
 
     return(
-        <div class="video-list-item-container">
-            <img src={imgUrl} />
-            <p>{video.snippet.title}</p>
+        <div className="video-list-item-container">
+            <a href={videoUrl}>
+                <img src={imgUrl} />
+                <p>{video.snippet.title}</p>
+            </a>
         </div>
     )
 }
