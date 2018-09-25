eb1f69a9b7eca575d3f3987c233ccf515f7aea78
npartovi <npartovi@gmail.com>
2018-09-24 22:35:50 -0700

    adding bigger thumbnails on mouseover

--- fgcvideos/client/src/VideoListItem.jsx
+++ fgcvideos/client/src/VideoListItem.jsx
@@ -3,14 +3,19 @@ import React from 'react'
 
 const VideoListItem = ({video}) => {
     console.log(video)
-    const imgUrl = video.snippet.thumbnails.medium.url
+    const mediumThumbnail = video.snippet.thumbnails.medium.url
+    const highThumbnail = video.snippet.thumbnails.high.url
     const videoId = video.id.videoId
     const videoUrl = `https://www.youtube.com/embed/${videoId}`;
 
     return(
-        <div className="video-list-item-container">
+        <div className="video-list-item">
             <a href={videoUrl}>
-                <img src={imgUrl} />
+                <img 
+                    src={mediumThumbnail}
+                    onMouseOver={(e) => e.currentTarget.src = highThumbnail}
+                    onMouseOut={(e) => e.currentTarget.src = mediumThumbnail} 
+                />
                 <p>{video.snippet.title}</p>
             </a>
         </div>
