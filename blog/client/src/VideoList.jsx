72e055655ebf175b5166a9db3848f729980dd8f1
npartovi <npartovi@gmail.com>
2018-09-24 17:42:35 -0700

    starting new project

---
+++ blog/client/src/VideoList.jsx
@@ -0,0 +1,45 @@
+import React, {Component} from 'react'
+import youtubeSearch from 'youtube-api-v3-search'
+import './App.css';
+// import _ from 'lodash'
+const API_KEY = 'AIzaSyBVOWDJTWMTdqWhkoguKVK1B9t3VWS68hA';
+
+class VideoList extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            term: "third strike",
+            videos: []
+        }
+
+        this.videoSearch = this.videoSearch.bind(this)
+    }
+
+    componentDidMount(){
+        this.videoSearch(this.state.term)
+    }
+
+    videoSearch(term){    
+        const options = {
+          q: term,
+          part: 'snippet',
+          type: "video"
+        }
+    
+        youtubeSearch(API_KEY, options)
+          .then((res) => {
+            this.setState({videos: res.items})
+          })
+      }
+
+
+
+    render(){
+        return(
+            <h1>Hello</h1>
+        )
+    }
+}
+
+export default VideoList
