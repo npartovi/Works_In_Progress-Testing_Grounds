65ea4b398130d97214d516ff1965c042821a2b2f
npartovi <npartovi@gmail.com>
2018-09-25 22:30:13 -0700

    added title to hover imgs

--- fgcvideos/client/src/VideoListItem.jsx
+++ fgcvideos/client/src/VideoListItem.jsx
@@ -27,12 +27,19 @@ class VideoListItem extends Component {
     render(){
         const { video } = this.props
         const mediumThumbnail = video.snippet.thumbnails.medium.url
+        const title = video.snippet.title
         const videoId = video.id.videoId
+        
 
         return(
             <React.Fragment>
                 <div onClick={this.showModal} className="video-list-item">
                     <img src={mediumThumbnail}/>
+                    <div class="tile__details">
+                        <div class="tile__title">
+                            {title}
+                        </div>
+                    </div>
                 </div>
                 <VideoModal show={this.state.modalOpen} hideModal={this.hideModal} videoId={videoId} />
             </React.Fragment>
