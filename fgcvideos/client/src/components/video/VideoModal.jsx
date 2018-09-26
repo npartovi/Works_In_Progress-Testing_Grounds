1762c31c35e5d7f6fac61f04f29ddf5cdbc70032
npartovi <npartovi@gmail.com>
2018-09-26 15:31:17 -0700

    moved youtube embed function into its own util

--- fgcvideos/client/src/components/video/VideoModal.jsx
+++ fgcvideos/client/src/components/video/VideoModal.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import YTEmbedLazyLoad from '../../util/youtube-embed-lazy-load'
 
 class VideoModal extends Component {
 
@@ -7,40 +8,10 @@ class VideoModal extends Component {
     }
 
     componentDidMount(){
-        this.youtubeEmbedLazyLoad()
-    }
-
-    youtubeEmbedLazyLoad(){
-
-        function labnolThumb(id) {
-            var thumb = '<img src="https://i.ytimg.com/vi/ID/hqdefault.jpg">',
-                play = '<div class="play"></div>';
-            return thumb.replace("ID", id) + play;
-        }
-    
-        function labnolIframe() {
-          console.log("hello")
-            var iframe = document.createElement("iframe");
-            var embed = "https://www.youtube.com/embed/ID?autoplay=1";
-            iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
-            iframe.setAttribute("frameborder", "0");
-            iframe.setAttribute("allowfullscreen", "1");
-            this.parentNode.replaceChild(iframe, this);
-        }
-
-        var div, n,
-                  v = document.getElementsByClassName("youtube-player");
-              for (n = 0; n < v.length; n++) {
-                  div = document.createElement("div");
-                  div.setAttribute("data-id", v[n].dataset.id);
-                  div.innerHTML = labnolThumb(v[n].dataset.id);
-                  div.onclick = labnolIframe;
-                  v[n].appendChild(div);
-              }
+        YTEmbedLazyLoad()
     }
 
     render(){
-
         const {videoId, hideModal, show } = this.props
         let showModalClassName = show ? 'modal display-block' : 'modal display-none'
 
