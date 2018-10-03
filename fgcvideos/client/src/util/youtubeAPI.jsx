2db9aec5085c28b9db82b9f00e2ff73c06e4983d
npartovi <npartovi@gmail.com>
2018-10-02 18:24:56 -0700

    added new project

--- fgcvideos/client/src/util/youtubeAPI.jsx
+++ fgcvideos/client/src/util/youtubeAPI.jsx
@@ -3,41 +3,31 @@ import keys from '../config'
 
 
 const youtubeAPI = (channels) => {
-
-
+    
     window.gapi.load("client", () => {
-        window.gapi.client.setApiKey(keys.youtubeAPIKey);
-        console.log("gapi loaded")
 
-        const batch = window.gapi.client.newBatch();
-        
+        window.gapi.client.setApiKey(keys.youtubeAPIKey);
+
         const searchRequest = (id) => {
             return window.gapi.client.request({
-              'path': 'youtube/v3/search',
+              'path': 'https://www.googleapis.com/youtube/v3/search',
               'params': {part: 'snippet', type: "video", channelId: id, order: "date", maxResults: 50}
              });
         };
 
         const request1 = searchRequest('UCIq8ow2OP3mRqWDVm7aRXCA')
         const request2 = searchRequest('UCT-WkUmMBrqDTXXAK4BOCbw')
+
+        const batch = window.gapi.client.newBatch();
         
         batch.add(request1)
         batch.add(request2)
          
-        batch
-            .then((res) => console.log(res))
+        batch.execute((res) => console.log(res))
 
-
-
-
-
-
-
-
-
+        
     })
-
-
 }
 
 export default youtubeAPI
+
