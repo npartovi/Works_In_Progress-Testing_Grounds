7afbb6cfa997c6fb14b68d00d7489f2f8f360735
npartovi <npartovi@gmail.com>
2018-10-03 14:25:09 -0700

    added layout and finished signout button

--- react-slack-clone/src/index.js
+++ react-slack-clone/src/index.js
@@ -6,7 +6,7 @@ import Login from './components/Auth/Login'
 import Register from './components/Auth/Register'
 import registerServiceWorker from './registerServiceWorker';
 import firebase from './firebase'
-import { setUser }from './actions/index'
+import { setUser, clearUser }from './actions/index'
 
 import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom'
 
@@ -14,22 +14,28 @@ import { createStore } from 'redux'
 import {Provider, connect} from 'react-redux'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import rootReducer from './reducers/index'
+import Spinner from './Spinner'
 
 const store = createStore(rootReducer, composeWithDevTools())
 
 class Root extends React.Component {
 
     componentDidMount(){
+        
         firebase.auth().onAuthStateChanged(user => {
             if(user){
                 this.props.setUser(user)
                 this.props.history.push('/')
+            }else{
+                this.props.history.push('/login')
+                this.props.clearUser()
             }
         })
     }
 
     render(){
-        return(
+        console.log(this.props.isLoading)
+        return this.props.isLoading ? <Spinner /> : (
                 <Switch>
                     <Route exact path="/" component={App} />
                     <Route path="/login" component={Login} />
@@ -40,7 +46,11 @@ class Root extends React.Component {
 }
 
 
-const RootWithAuth = withRouter(connect(null, {setUser})(Root))
+const mapStateToProps = (state) => ({
+    isLoading: state.user.isLoading
+})
+
+const RootWithAuth = withRouter(connect(mapStateToProps, {setUser, clearUser})(Root))
 
 ReactDOM.render(<Provider store={store}><Router><RootWithAuth/></Router></Provider>, document.getElementById('root'));
 registerServiceWorker();
