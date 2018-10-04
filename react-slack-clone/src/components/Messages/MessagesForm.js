2f6b49247cb3a8d8a0d6b663872d38ccc45ca1bb
npartovi <npartovi@gmail.com>
2018-10-03 22:03:59 -0700

    end of day commit

--- react-slack-clone/src/components/Messages/MessagesForm.js
+++ react-slack-clone/src/components/Messages/MessagesForm.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import uuidv4 from  'uuid/v4'
 import {Button, Input, Segment} from 'semantic-ui-react'
 import FileModal from './FileModal'
+import ProgressBar from './ProgressBar'
 import firebase from '../../firebase'
 
 class MessageForm extends Component {
@@ -146,7 +147,7 @@ class MessageForm extends Component {
 
     render(){
 
-        const {errors, message, loading, modal} = this.state
+        const {errors, message, loading, modal, uploadState, percentUploaded } = this.state
 
         return(
             <Segment className="message__form">
@@ -180,12 +181,16 @@ class MessageForm extends Component {
                         labelPosition="right"
                         icon="cloud upload"
                     />
-                    <FileModal 
-                        modal={modal} 
-                        closeModal={this.closeModal}
-                        uploadFile={this.uploadFile}
-                    />
                 </Button.Group>
+                <FileModal 
+                    modal={modal} 
+                    closeModal={this.closeModal}
+                    uploadFile={this.uploadFile}
+                />
+                <ProgressBar 
+                    uploadState={uploadState}
+                    percentUploaded={percentUploaded}
+                />
             </Segment>
         )
     }
