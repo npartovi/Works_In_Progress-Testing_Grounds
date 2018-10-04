3cf491484c7f7b466056612072db880f425d60a1
npartovi <npartovi@gmail.com>
2018-10-03 23:46:57 -0700

    batch processing makes no sense or I am jsut stupid

--- fgcvideos/client/src/util/youtubeAPI.jsx
+++ fgcvideos/client/src/util/youtubeAPI.jsx
@@ -1,32 +1,46 @@
 import channelList from './channel-id-list'
 import keys from '../config'
+import axios from 'axios'
 
 
 const youtubeAPI = (channels) => {
     
-    window.gapi.load("client", () => {
+    // window.gapi.load("client", () => {
+    //     window.gapi.client.setApiKey(keys.youtubeAPIKey);
+    //     window.gapi.client.load('youtube', 'v3', () => {
 
-        window.gapi.client.setApiKey(keys.youtubeAPIKey);
+    //         const searchRequest = (id) => {
+    //             return window.gapi.client.request({
+    //               'path': 'youtube/v3/search',
+    //               'params': {part: 'snippet', type: "video", channelId: id, order: "date", maxResults: 50}
+    //              });
+    //         };
 
-        const searchRequest = (id) => {
-            return window.gapi.client.request({
-              'path': 'https://www.googleapis.com/youtube/v3/search',
-              'params': {part: 'snippet', type: "video", channelId: id, order: "date", maxResults: 50}
-             });
-        };
+    //         const request1 = searchRequest('UCIq8ow2OP3mRqWDVm7aRXCA')
+    //         const request2 = searchRequest('UCT-WkUmMBrqDTXXAK4BOCbw')
 
-        const request1 = searchRequest('UCIq8ow2OP3mRqWDVm7aRXCA')
-        const request2 = searchRequest('UCT-WkUmMBrqDTXXAK4BOCbw')
+    //         const batch = window.gapi.client.newBatch();
+            
+    //         batch.add(request1)
+    //         batch.add(request2)
 
-        const batch = window.gapi.client.newBatch();
-        
-        batch.add(request1)
-        batch.add(request2)
-         
-        batch.execute((res) => console.log(res))
+    //         batch
+    //             .then((res) => console.log(res))
+            
+           
+    //     })
+    // })
 
-        
-    })
+    // const id1 = "UCIq8ow2OP3mRqWDVm7aRXCA"
+    // const id2 = "UCT-WkUmMBrqDTXXAK4BOCbw"
+
+    // axios.get(`https://www.googleapis.com/youtube/v3/search?key=${keys.youtubeAPIKey}&part=snippet&type=video&maxResults=50&channelId=${id1},${id2}`)
+    //     .then(res => {
+    //         console.log(res)
+    //     })
+    //     .catch(err => console.error(err))
+
+
 }
 
 export default youtubeAPI
