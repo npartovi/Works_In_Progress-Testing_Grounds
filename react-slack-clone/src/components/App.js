314949d81d5499cf498ed279b284e5cd3dca48d0
npartovi <npartovi@gmail.com>
2018-10-04 17:53:14 -0700

    metadata component started, 50% done

--- react-slack-clone/src/components/App.js
+++ react-slack-clone/src/components/App.js
@@ -27,7 +27,7 @@ const App = ({currentUser, currentChannel, isPrivateChannel}) => (
     </Grid.Column>
 
     <Grid.Column width={4}>
-      <MetaPanel />
+      <MetaPanel isPrivateChannel={isPrivateChannel} key={currentChannel && currentChannel.id} />
     </Grid.Column>
 
   </Grid>
