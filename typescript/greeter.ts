c4ee6c60cd39c5d7c2fb414d408b2badd0c14f4e
npartovi <npartovi@gmail.com>
2018-10-31 15:13:29 -0700

    type script

---
+++ typescript/greeter.ts
@@ -0,0 +1,14 @@
+
+
+interface Person {
+    firstName: string,
+    lastName: string
+}
+
+function greeter(person: Person){
+    return 'Hello' + person.firstName + person.lastName
+}
+
+let user = {firstName: "jane", lastName: "doe"}
+
+document.body.innerHTML = greeter(user)
